014b5bf13d822217bf40b07c71807851e6f5c1f6
Dawid <daw8hub@gmail.com>
2021-05-02 12:58:27 +0200

    "Results not found" text css changed

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { storage } from "../firebase";
 import "./Dashboard.css";
+import { useHistory } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
 import defaulAvatar from "../images/defaultuser.png";
-export default function Result({ user }) {
-  const [profilePic, setprofilePic] = useState(null);
-
+export default function Result({ resultKey, user }) {
+  const history = useHistory();
+  const profileRedirect = (e) => {
+    history.replace("/home/u/" + resultKey);
+  };
   {
     return (
-      <div className="searchResults__card">
+      <div className="searchResults__card" onClick={profileRedirect}>
         <div className="searchResults__cardBod">
-          <Avatar
-            src={user.avatarUrl}
-            className="searchResults__avatar"
-          ></Avatar>
+          {user.avatarUrl ? (
+            <Avatar
+              src={user.avatarUrl}
+              className="searchResults__avatar"
+            ></Avatar>
+          ) : (
+            <Avatar
+              src={defaulAvatar}
+              className="searchResults__avatar"
+            ></Avatar>
+          )}
           <div className="searchResults__username">{user.username}</div>
+
           <div className="searchResults__bio">{user.bio}</div>
         </div>
       </div>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,7 +4,7 @@ import "./Dashboard.css";
 import { Button } from "@material-ui/core";
 import { useParams } from "react-router-dom";
 import Result from "./Result";
-
+import InfoIcon from "@material-ui/icons/Info";
 export default function SearchResults() {
   const { word } = useParams();
   const [users, setUsers] = useState(null);
@@ -16,15 +16,21 @@ export default function SearchResults() {
       .limit(8)
       .get()
       .then((snapshot) => {
-        setUsers(
-          snapshot.docs.map((doc) => ({
-            id: doc.id,
-            user: doc.data(),
-          }))
-        );
+        if (!snapshot.empty) {
+          setUsers(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              user: doc.data(),
+            }))
+          );
+        }
         setLastVisible(snapshot.docs[snapshot.docs.length - 1]);
       });
   }, []);
+
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
 
   function addPostsScroll(e) {
     if (lastVisible) {
@@ -50,8 +56,21 @@ export default function SearchResults() {
 
   return (
     <div className="searchResults">
-      {users &&
-        users.map(({ id, user }) => <Result key={id} user={user}></Result>)}
+      {users ? (
+        users.map(({ id, user }) => (
+          <Result resultKey={id} user={user}></Result>
+        ))
+      ) : (
+        <div className="searchResults__notFound">
+          <div className="searchResults__notFoundUp">
+            <InfoIcon></InfoIcon>
+            Nie znaleziono użytkownika
+          </div>
+          <div className="searchResults__notFoundDown">
+            Pasującego do: "{word}"
+          </div>
+        </div>
+      )}
       <div className="searchResults__loadButton">
         {lastVisible && (
           <Button
